16eebaad84db73c707ca3f4f14fa204033783db4
Italo Carlo <italocarlo@nti.ufal.br>
2022-03-22 23:11:13 -0300

    Challenge 01 implementations

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (! (newTaskTitle == "")) {
+      const newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks([...tasks, newTask]);
+    }
+    setNewTaskTitle("");
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+            
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }:task);
+
+    
+    setTasks([...newTasks]);    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const tasksList = tasks.filter(element => element.id !== id);
+    setTasks([...tasksList]);
   }
 
   return (
